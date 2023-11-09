c7ba36bfda70f60b17fcf9ba0f136967eb431dc5
Logan King <logan.ramzie.king@gmail.com>
2023-11-08 20:37:18 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let myName = "Tyler";
+const greetMe = () => {
+    let myGreeting = "Hello, ";
+    console.log(myGreeting + myName + "!");
+};
+console.log(greetMe)
+
+let diameter= 20
+if (diameter > 0) {
+    let pi = 3.14;
+    let circumference = pi * diameter;
+console.log("the circumference is: " + circumference);
+}
+
